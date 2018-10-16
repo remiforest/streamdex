59877a022eabd3df50a5fbd5c80b614be606db69
remiforest <remi.forest@akilio.com>
2018-10-16 14:12:58 +0200

    ingest ok

--- ingest.py
+++ ingest.py
@@ -3,8 +3,10 @@
 import json
 import sys
 import os
+import requests
 import subprocess
 
+from requests.auth import HTTPBasicAuth
 from confluent_kafka import Producer
 
 # Create producers dict
@@ -14,23 +16,18 @@ streams = {}
 # load object
 
 def create_stream(path,name):
-    return os.popen("maprcli stream create -path " + path + name + " -produceperm p -consumeperm p -topicperm p").read()    
+    escaped_path = path.replace('/','%2F')
+    req = requests.get('https://127.0.0.1:8443/rest/stream/create?path=' + escaped_path + name,verify=False,auth=HTTPBasicAuth('mapr', 'mapr'))
+    return req.text    
+    # return os.popen("maprcli stream create -path " + path + name + " -produceperm p -consumeperm p -topicperm p").read()    
 
-def create_topic(stream,name):
-    return os.popen("maprcli stream topic create -path " + stream + " -topic " + name).read()    
-
-def get_topics(path,name):
-    child = subprocess.Popen("maprcli stream topic list -path " + path + name + " -json",shell=True,stdout=subprocess.PIPE)
-    output = child.communicate()[0]
-    out_doc = json.loads(output)
-    topics = []
-    for topic in out_doc["data"]:
-        topics.append(topic["topic"])
-    return(topics)
-
-
-# Ajouter la partie DB pour le stockage des objets de base
-# key = object_id
+def format_value(value):
+    value = value.replace("'","_") # Handling names with '
+    value = value.replace(" ","_") # Handling names with spaces
+    value = value.replace(",","_") # Handling names with ,
+    value = value.replace(";","_") # Handling names with ;
+    value = value.replace("@","_") # Handling names with ,
+    return value
 
 path = "/dev/index/streamdex/"
 fullpath = "/mapr/demo.mapr.com/dev/index/streamdex/"
@@ -49,53 +46,40 @@ for round in range(2):
         del doc["ts"]
 
         for key, value in doc.iteritems():
-            print("processing {}:{}".format(key,value))
+            # print("processing {}:{}".format(key,value))
             try:
                 # test is value is a number
                 num_val = float(value)
-                print("{} is a number. Skipping.".format(value))
+                # print("{} is a number. Skipping.".format(value))
                 continue
-            except Exception, e:
-                print(e)
+            except Exception as e:
+                # print(e)
                 # if value is not a number
                 # insert object_id into key_value stream
-                print("{} is text.".format(value))
-                value = value.replace("'","_") # Handling names with ' 
+                # print("{} is text.".format(value))
+                value = format_value(value)
                 new_doc = {"id" : object_id, "ts" : ts} # doc to be insterted in the stream
                 if not key in streams: # if the stream isn't referenced it's created
-                    print("unknown stream, testing")
+                    # print("unknown stream, testing")
                     streams[key] = {}
-                    streams[key]["topics"] = []
+                    # streams[key]["topics"] = []
                     try:
                         # testing if stream exists
                         if os.path.islink(fullpath + key): # stream exists
                             print("stream exists")
-                            topics = get_topics(path,key)
-                            print("existing topics for stream {}".format(path + key))
-                            print(topics)
-                            streams[key]["topics"] = topics
                         else:
                             print("creating stream {}".format(key))
                             out = create_stream(path,key)
                         streams[key]["producer"] = Producer({'streams.producer.default.stream': path + key})
-                    except Exception, e:
+                    except Exception as e:
                         print("create stream failed")
                         print(e)
                         break
                 p = streams[key]["producer"]
-
-                if not value in streams[key]["topics"]: # if topic doesn't exists it's created
-                    try:
-                        print("creating topic {}:{}".format(key,value))
-                        out = create_topic(path + key, value)
-                        streams[key]["topics"].append(value)
-                    except Exception, e:
-                        print("create topic failed")
-                        print(e)
           
-                print("producing {}".format(json.dumps(new_doc)))
+                # print("producing {}".format(json.dumps(new_doc)))
                 p.produce(value, json.dumps(new_doc).encode('utf-8'))
                 count += 1
-                print("produced")
+                # print("produced")
 
 
--- rest.py
+++ rest.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 import requests
+from requests.auth import HTTPBasicAuth
 
-req = requests.get('https://127.0.0.1:8443/rest/stream/topic/list?path=%2Fdev%2Findex%2Fstreamdex%2Fcountry',verify=False)
+req = requests.get('https://127.0.0.1:8443/rest/stream/topic/list?path=%2Fdev%2Findex%2Fstreamdex%2Fcountry',verify=False,auth=HTTPBasicAuth('mapr', 'mapr'))
 print(req.text)
